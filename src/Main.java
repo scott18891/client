2243b2f935a733b182e25f0a047426291ecc1edd
Scott18891 <ssmoke2020@gmail.com>
2019-08-03 16:12:50 -0500

    Making client a p2p

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,56 @@
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class Main {
+public class Main  {
 
     public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        String message = null;
         InetAddress host = InetAddress.getLocalHost();
-        Socket socket = new Socket(host, 8080);
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ServerSocket serverSocket = new ServerSocket(4222);
+        Runnable runnable =
+                () -> {
+                    try {
+                        System.out.println("Listening on port 4222");
+                        displayMessage(serverSocket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                };
+        Thread thread = new Thread(runnable);
+        thread.start();
 
-        out.writeUTF("Message");
+        while (null == message || !message.equals("disconnect")) {
+            message = scan.nextLine();
+            sendMessage(host, message);
+        }
+        scan.close();
+    }
+
+    public static void sendMessage(InetAddress host, String message) throws IOException {
+        Socket socket = new Socket(host, 4222);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        out.writeUTF(message);
         out.flush();
         out.write(-1);
         out.flush();
         out.close();
+    }
 
+    public static void displayMessage(ServerSocket serverSocket) throws IOException {
+        boolean disconnect = false;
+        while (!disconnect) {
+            ObjectInputStream ois = new ObjectInputStream(serverSocket.accept().getInputStream());
+            String sentMessage = ois.readUTF();
+            System.out.println("Message: " + sentMessage);
+            if(sentMessage.equals("disconnect")){
+                disconnect = true;
+            }
+        }
     }
 }
